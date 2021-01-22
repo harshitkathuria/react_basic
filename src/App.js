a860c892cdb33feae8c73531cea838a831001c5f
harshitkathuria <harshitkathuria14@gmail.com>
2021-01-22 20:46:27 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+// App component (can be a class)
+function App() {
+  return (
+    // JSX - Javascript Syntax Extension (similar to HTML)
+    // replace class with className
+    // JSX must return a single parent element
+    <div className="App">
+      <h1>First React Application </h1>
+      <h2>By Harshit Kathuria</h2>
+    </div>
+  );
+}
+
+export default App;
